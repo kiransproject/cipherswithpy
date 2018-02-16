aa2ad2af0868f58e89e8b4ee41c29160333fdcc8
kpmistry <kiran.mistry@live.co.uk>
2018-02-16 18:20:33 +0000

    working affine Encrypt

---
+++ E_15_Affine_Cipher.py
@@ -0,0 +1,41 @@
+# Affine Ciphea
+
+import  E_9A_decrypt_transposition_cipher, cryptomath, sys
+
+ascii_length = 128 #127 is the final value startinga at 0
+
+def main():
+    nb = E_9A_decrypt_transposition_cipher.fetchmessage()
+    key =E_9A_decrypt_transposition_cipher.fetchkey()
+    mode =E_9A_decrypt_transposition_cipher.fuctionality() 
+    if (mode):
+        print(encrypt(nb, key, mode))
+  #  else:
+   #     newmes = decrypt(nb, key, mode)
+
+
+def getKeySections(kys):
+    keyA = kys // ascii_length #gives integer division 
+    keyB = kys % ascii_length #gives remaninder 
+    return (keyA, keyB)
+
+def encrypt(message, kys, mode):
+    KeyA, KeyB = getKeySections(kys)
+    checkKeys(KeyA, KeyB, mode)
+    ciphertext = ''
+    for symbol in message:
+        ciphertext += chr(((ord(symbol) * KeyA)+KeyB) % ascii_length)
+    return ciphertext
+
+def checkKeys(key1, key2, mode):
+    if ((mode) and (key1 == 1)):
+        sys.exit('The affine cipher becomes incredibly weka when A is set to 1. Chose a different key.')
+    if ((mode) and (key2 == 0)):
+        sys.exit('The affine cipher becomes incredibly weka when B is set to 0. Chose a different key.')
+    if ( key1< 0 or key2 <0 or key2> (ascii_length-1)):
+        sys.exit(' Key A and B must be greater than 0 and key B must be less than %s ' % (ascii_length-1))
+    if (cryptomath.gcd(key1, ascii_length) != 1):
+        sys.exit(' Key A and the symbol set size %s are not relatively prime ' % ascii_length)
+
+if __name__ == '__main__':
+    main()
